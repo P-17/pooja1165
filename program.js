dcaeea5e9b1a6383c2d453fe55c40c9853cecc66
jinkal17 <jinkal.bhimani17@gmail.com>
2019-06-26 10:19:37 +0530

    first step

---
+++ program.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+//Reusable controllers
+var employeeController=function (req, res) {
+  console.log("CAlling rest api");
+  var employee=[
+            {firstName:'pooja',lastName:'pawar',age:23,status:'manager'},
+			{firstName:'prachi',lastName:'patil',age:23,status:'employee'},
+            {firstName:'gorprit',lastName:'shikh',age:23,status:'sales'},
+            
+            
+      ];
+  res.send(employee);
+};
+
+// setting router to map requests  with controllers
+
+app.get('/employee',employeeController );
+
+var server = app.listen(2000, function () {
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://localhost:2000", host, port)
+})
